b367f9f71611d52d15a0926b4ba0a9ef081a575c
Laura Vedovatto <laura.vedovatto@gmail.com>
2022-04-15 11:10:00 -0300

    protocolos itau

--- TableViewTests/TableViewTests/ViewController.swift
+++ TableViewTests/TableViewTests/ViewController.swift
@@ -8,13 +8,80 @@
 
 import UIKit
 
+class Empresa {
+    var chefe: Funcionario?
+    var funcionarios = [Funcionario]()
+    
+    func comecarAlmoco() {
+        funcionarios.forEach({ $0.almocar() })
+    }
+    
+    func contratarChefe(chefe: Funcionario) {
+        self.chefe = chefe
+        self.funcionarios.append(chefe)
+    }
+}
+
+protocol Funcionario {
+    func almocar()
+    func trabalhar()
+}
+
+class TechLead: Funcionario {
+    func almocar() {
+        print("O chefe está almoçando...")
+    }
+    
+    func trabalhar() {
+        
+    }
+}
+
+class Programador: Funcionario {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func almocar() {
+        print(name + " está almoçando...")
+    }
+    
+    func trabalhar() {
+        
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
+        let itau = Empresa()
+        itau.contratarChefe(chefe: TechLead())
+        itau.funcionarios.append(Programador(name: "Minatti"))
+
+        itau.comecarAlmoco()
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Minatti"
+        
+        return cell
+    }
+}
